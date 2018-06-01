6c6f935646f0f0de0d83a8483c4237ac608452f2
Unknown <q.vanveen@student.fontys.nl>
2018-06-01 15:07:50 +0200

    Dashboard Logica
    Logica voor:
    -Lijsten maken
    -Overzicht lijst gebruiker
    -Overzicht openbare lijsten

---
+++ SLT_Site/Business/Dashboard/CreatListLogic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataModellen;
+using DAL;
+
+namespace Business.Dashboard
+{
+    public class CreatListLogic
+    {
+        DataBaseCalls db = new DataBaseCalls();
+        private List<Woord> lijst = new List<Woord>();
+
+        public Lijst CreateList(string s, string username)
+        {
+            string[] stringArray = s.Split('/');
+            string titel = stringArray[0];
+            string[] opties = stringArray[1].Split('?');
+            string soort = stringArray[2];
+            int isPublic = 0;
+            if (opties[1].Split('=')[1] == "true")
+            {
+                isPublic = 1;
+            }
+            string[] stringLijst = opties[0].Split('|');
+            foreach (string t in stringLijst)
+            {
+                if (t != "")
+                {
+                    string[] w = t.Split('-');
+                    Woord k = new Woord() { Begrip = w[0], Betekenis = w[1] };
+                    lijst.Add(k);
+                }
+            }
+            Lijst l = new Lijst{ Gebruikersnaam = username, Datum = String.Format("{0:dd/MM/yyyy}", DateTime.Today), isPublic = isPublic, Soort = soort, Titel = titel, WoordenLijst = lijst };
+            db.InsertLijst(l);
+            return l;
+        }
+    }
+}
---
+++ SLT_Site/Business/Dashboard/ListOverviewLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using DataModellen;
+using DAL;
+
+namespace Business.Dashboard
+{
+    public class ListOverviewLogic
+    {
+        DataBaseCalls db = new DataBaseCalls();
+
+        public Lijst GeefLijst(string lijstnaam, string username)
+        {
+            int id = db.GetLijstID(username, lijstnaam);
+            DataTable data = db.Getlijst(id);
+            List<Woord> lijst = new List<Woord>();
+            foreach (DataRow dr in db.GetWoordenFromLijst(id).Rows)
+            {
+                Woord w = new Woord()
+                {
+                    Begrip = dr[0].ToString(),
+                    Betekenis = dr[1].ToString()
+                };
+                lijst.Add(w);
+            }
+
+            Lijst l = new Lijst()
+            {
+                Datum = data.Rows[0][3].ToString(),
+                Gebruikersnaam = data.Rows[0][1].ToString(),
+                isPublic = Convert.ToInt32(data.Rows[0][6]),
+                Soort = data.Rows[0][2].ToString(),
+                Titel = data.Rows[0][4].ToString(),
+                WoordenLijst = lijst
+
+            };
+            return l;
+        }
+
+    }
+}
