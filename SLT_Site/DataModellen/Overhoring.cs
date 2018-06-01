cd0adc43122e72b0b8699258a8b117af1677e45a
Unknown <q.vanveen@student.fontys.nl>
2018-06-01 15:20:25 +0200

    Overhoring Logic
    -Gebruiker kan zichzelf overhoren

---
+++ SLT_Site/DataModellen/Overhoring.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModellen
+{
+    public class Overhoring
+    {
+        private string mSoort;
+        private bool mRandom;
+        private string mVraag;
+        private int mAantalFout;
+        private int mAantalGoed;
+        private List<Woord> mWoordenLijst;
+
+        public string Soort{get { return mSoort;}set { mSoort = value; }}
+        public bool Random { get { return mRandom; } set { mRandom = value; } }
+        public string Vraag { get { return mVraag; } set { mVraag = value; } }
+        public int AantalFout { get { return mAantalFout; } set { mAantalFout = value; } }
+        public int AantalGoed { get { return mAantalGoed; } set { mAantalGoed = value; } }
+        public List<Woord> WoordenLijst { get { return mWoordenLijst; } set { mWoordenLijst = value; } }
+
+
+    }
+}
